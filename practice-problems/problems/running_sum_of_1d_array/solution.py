9b5430dd7c168e1d8340f4f4828d0c59fc28cd97
Oli Eugenio <46060175+OEUG99@users.noreply.github.com>
2023-05-08 15:03:57 -0400

    Delete practice-problems directory

--- practice-problems/problems/running_sum_of_1d_array/solution.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        result = []
-        for x in range(len(nums)):
-            result.append(sum(nums[:x+1]))
-        
-        return result
-        
